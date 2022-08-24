0c64d6389003f76735722380edcda3918facd7d8
emil-westling <98067263+emil-westling@users.noreply.github.com>
2022-08-24 16:17:11 -0400

    Implemented a few methods

--- src/utilities/ParallelArrayDictionary.java
+++ src/utilities/ParallelArrayDictionary.java
@@ -1,11 +1,69 @@
 package utilities;
 
-public class ParallelArrayDictionary
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+public class ParallelArrayDictionary<Key, Value> implements Map<Key, Value>
 {
 
-	public ParallelArrayDictionary()
-	{
-		// TODO Auto-generated constructor stub
+	protected ArraySet<Key> _keys;
+	protected ArrayList<Value> _values;
+	
+	@Override
+	public int size() { return _keys.size(); }
+	
+	@Override
+	public boolean isEmpty() { return _keys.isEmpty(); }
+	
+	@Override
+	public boolean containsKey(Object key) { return _keys.contains(key); }
+	
+	@Override
+	public boolean containsValue(Object value) { return _values.contains(value); }
+	
+	// TODO
+	@Override
+	public Value get(Object key) { return _values.get(key); }
+
+	@Override
+	public Value put(Key key, Value value) { 
+		Value oldValue = _values.get(key);
+		_values.set(key, value);
+		// TODO
 	}
+	
+	@Override
+	public Value remove(Object key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void putAll(Map<? extends Key, ? extends Value> m) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public Set<Key> keySet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public Collection<Value> values() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public Set<Entry<Key, Value>> entrySet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 
 }
