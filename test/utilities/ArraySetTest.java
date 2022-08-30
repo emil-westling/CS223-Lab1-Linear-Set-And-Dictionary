aea938bde25c7cc6c7ec41b5fcf3d7359cd38431
emil-westling <98067263+emil-westling@users.noreply.github.com>
2022-08-29 22:47:33 -0400

    test method for size() in ArraySet

--- test/utilities/ArraySetTest.java
+++ test/utilities/ArraySetTest.java
@@ -1,6 +1,8 @@
 package utilities;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotSame;
 
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
@@ -9,6 +11,29 @@ class ArraySetTest<E>
 {
 	//ArraySet<E> tester = new ArraySet<E>();
 	ArraySet<Integer> tester = new ArraySet<Integer>();
+	
+	private static final int SIZE_OF_LIST_TEST = 5;
+	
+	/**
+	 * checks size() and isEmpty() methods on both empty sets and nonempty sets. 
+	 */
+	@Test
+	public void testSize() {
+		ArraySet<Integer> sizeTest = new ArraySet<Integer>();
+		
+		// size 0
+		assertTrue(sizeTest.size() == 0);
+		assertTrue(sizeTest.isEmpty() == true);
+		
+		for (int i = 0; i < SIZE_OF_LIST_TEST; i++) {
+			tester.add(i);
+			assertTrue(sizeTest.size() == i + 1);
+			assertFalse(sizeTest.isEmpty());
+			assertNotSame(0, sizeTest.size());
+		}
+		
+		assertTrue(sizeTest.size() == 5);
+	}
 	
 	@Test
 	void testArraySetCollectionOfE()
